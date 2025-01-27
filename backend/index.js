50e0e7b5457871613fbf64c2a2a94f1a9912f21f
Ayush Mittal <ayushmittal1100@gmail.com>
2025-01-27 14:04:05 +0530

    fix cors issue

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,9 @@ const corsOptions = {
     credentials: true,
     optionsSuccessStatus: 200,
     allowedHeaders: ['Content-Type', 'Authorization'],
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    preflightContinue: false,
+    exposedHeaders: ['Access-Control-Allow-Origin', '*']
 };
 
 app.use(cors(corsOptions));
